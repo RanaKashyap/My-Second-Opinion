68f21eab94f133e2fc0ebd72e3ba92e5436b2c71
Kashyap Rana <kashyaprana202@gmail.com>
2024-04-28 19:50:21 -0400

    static side nav bar

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   var adminDashboardLink = document.getElementById("adminDashboardLink");
+//   var adminDashboardImage = document.getElementById("adminDashboardImage");
+
+//   adminDashboardLink.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     // Remove active class from all nav items
+//     document.querySelectorAll(".sidebar a").forEach(function (item) {
+//       item.classList.remove("active");
+//     });
+//     // Add active class to clicked nav item
+//     adminDashboardLink.classList.add("active");
+//     // Change image source
+//     adminDashboardImage.src = "./images/nav-icons/active_home_dash.png";
+//   });
+
+//   // Add similar event listeners for other nav items
+// });
