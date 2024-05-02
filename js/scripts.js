f95d6a7d36b9def38987fd88e8dbdf5e35769996
Kashyap Rana <kashyaprana202@gmail.com>
2024-05-01 20:55:20 -0400

    responsive sidebar + pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,10 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   var adminDashboardLink = document.getElementById("adminDashboardLink");
-//   var adminDashboardImage = document.getElementById("adminDashboardImage");
+const sideMenu = document.querySelector('#side_bar');
+const menuBtn = document.querySelector('#menu_bar');
+const closeBtn = document.querySelector('#close_btn');
 
-//   adminDashboardLink.addEventListener("click", function (event) {
-//     event.preventDefault();
-//     // Remove active class from all nav items
-//     document.querySelectorAll(".sidebar a").forEach(function (item) {
-//       item.classList.remove("active");
-//     });
-//     // Add active class to clicked nav item
-//     adminDashboardLink.classList.add("active");
-//     // Change image source
-//     adminDashboardImage.src = "./images/nav-icons/active_home_dash.png";
-//   });
-
-//   // Add similar event listeners for other nav items
-// });
+menuBtn.addEventListener('click', () => {
+  sideMenu.style.display = "block"
+})
+closeBtn.addEventListener('click', () => {
+  sideMenu.style.display = "none"
+})
