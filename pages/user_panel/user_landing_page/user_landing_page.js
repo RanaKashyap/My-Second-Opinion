d586adf249aa831d70ddc2735c66780d35fa0153
Kashyap Rana <kashyaprana202@gmail.com>
2024-05-10 18:49:07 -0400

    main dash landing page

--- pages/user_panel/user_landing_page/user_landing_page.js
+++ pages/user_panel/user_landing_page/user_landing_page.js
@@ -1,7 +1,8 @@
 // Sample data
 const data = [
-  { service: 'Second Opinion', date: '12/12/2024', },
-  { service: 'Find a dentist', date: '12/12/2024', },
+  { id: 'Te154235', date: '12/12/2023', deadline: '5 Days', status: 'Accept/Decline', icon: 'check' },
+  { id: 'Te154235', date: '12/12/2023', deadline: '5 Days', status: 'In Progress', icon: 'check' },
+  { id: 'Te154235', date: '12/12/2023', deadline: '5 Days', status: 'Completed', icon: 'eye' },
   // Add more data objects as needed
 ];
 
@@ -11,11 +12,14 @@ function createTableRows(data) {
 
   data.forEach(item => {
     const row = document.createElement('tr');
+    const iconHtml = item.icon === 'eye' ? '<i class="fa-regular fa-eye"></i>' : '<i class="fa-regular fa-pen-to-square"></i>';
+
     row.innerHTML = `
-      <td>${item.service}</td>
+      <td>${item.id}</td>
       <td>${item.date}</td>
-      <td><button class="add-record-btn">Add dental Records</button></td>
-      <td><button><i class="fa-solid fa-trash"></i></button></td>
+      <td>${item.deadline}</td>
+      <td><div class="status">${item.status}</div></td>
+      <td><button>${iconHtml}</button></td>
     `;
     tableBody.appendChild(row);
   });
